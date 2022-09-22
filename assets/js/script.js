5dc94bccbbe30ab33bb081e12b7f7e37a2dba6b2
01anto <01anto@live.ie>
2022-09-22 20:44:08 +0000

    Add code to runGame function that changes the computer-image source depending on computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ for (let button of buttons) {
 function runGame(playerChoice) {
     playerImage.src = `../assets/images/${choicesArray[playerChoice]}.jpg`;
     playerImage.alt = choicesArray[playerChoice];
+
     let computerChoice = Math.floor(Math.random() * 3);
-    console.log(computerChoice);
+    computerImage.src = `../assets/images/${choicesArray[computerChoice]}.jpg`;
+    computerImage.alt = choicesArray[computerChoice];
 }
